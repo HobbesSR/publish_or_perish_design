cbcdbe5c6530217b1e09526f2da3f10809434390
Corey Smith <20545418+HobbesSR@users.noreply.github.com>
2022-08-16 22:23:02 -0500

    Setup so habitat cards are now generated with images.

--- dinocards.py
+++ dinocards.py
@@ -19,6 +19,68 @@ CARDS = [
     {"habitat":[1,0,1,0,0,1,0,1], "dino":[0,1,0,1,0,1,0,0]}
 ]
 
+HABITAT_NAMES = [
+    "Island",
+    "Lagoon",
+    "Swamp",
+    "Floodplain",
+    "Prarie",
+    "River",
+    "Riparian Forest",
+    "Coniferous Forest",
+    "Marsh",
+    "Lake",
+    "Delta",
+    "Savanna",
+    "Coast",
+    "Mixed Forest",
+    "Deciduous Forest",
+    "Desert Plain",
+    "Scrubland",
+    "Wetland"
+]
+
+HABITAT_DIRECTORIES = [
+    "island",
+    "lagoon",
+    "swamp",
+    "floodplain",
+    "prarie",
+    "river",
+    "riparian_forest",
+    "coniferous_forest",
+    "marshland",
+    "lake",
+    "delta",
+    "savanna",
+    "coast",
+    "mixed_forest",
+    "deciduous_forest",
+    "desert_plain",
+    "scrubland",
+    "wetlands"
+]
+
+HABITAT_IMAGE_FILENAMES = [
+    "dinos(4)_1.png",
+    "dinos(6)_7.png",
+    "dinos(7)_7.png",
+    "dinos(8)_6.png",
+    "dinos(9)_1.png",
+    "dinos(10)_4.png",
+    "dinos(2)_4.png",
+    "dinos(11)_6.png",
+    "dinos(12)_3.png",
+    "dinos(14)_9.png",
+    "dinos(16)_6.png",
+    "dinos(17)_7.png",
+    "dinos(18)_1.png",
+    "dinos(24)_6.png",
+    "dinos(25)_15.png",
+    "dinos(26)_2.png",
+    "dinos(27)_3.png",
+    "dinos(28)_1.png"
+]
 
 def applyCardToRequirements(cardIndex, requirementsIn):
     card = CARDS[cardIndex]["habitat"]
@@ -120,25 +182,41 @@ resourceLetters = {
 for cardIndex in range(len(CARDS)):
     card = CARDS[cardIndex]
     habitat = card["habitat"]
-    print("Habitat" + str(cardIndex+1), end =",")
+    # habitat name
+    print(HABITAT_NAMES[cardIndex], end =",")
+
+    # habitat image path
+    print(HABITAT_DIRECTORIES[cardIndex] + "/" + HABITAT_IMAGE_FILENAMES[cardIndex], end = ",")
+
+    # habitat environment traits
     for traitIndex in range(5):
     	for i in range(habitat[traitIndex]):
     	   print(resourceLetters[traitIndex], end = "")
     print(",", end = "")
+
+    # habitat prey traits
     for traitIndex in range(5,8):
     	for i in range(habitat[traitIndex]):
     	   print(resourceLetters[traitIndex], end = "")
     print(",", end = "")
+
+    # dinosaur name
     dino = card["dino"]
     print("Dino" + str(cardIndex+1), end =",")
+
+    # dinosaur environment trait requirements
     for traitIndex in range(5):
     	for i in range(dino[traitIndex]):
     	   print(resourceLetters[traitIndex], end = "")
     print(",", end = "")
+
+    # dinosaur prey trait requirements
     for traitIndex in range(5,8):
     	for i in range(dino[traitIndex]):
     	   print(resourceLetters[traitIndex], end = "")
     print(",", end = "")
+
+    # dinosaur point value
     solutionRatioScale = 1 + (totalSolutionsRatiosByMax[i]**2)
     print(round((totalCardsAverages[cardIndex] / totalCardsAverage) * totalCardsAverages[cardIndex] * solutionRatioScale))
 
